024b59231b34a7c802568544303079f19d61ea5c
noman68939 <ushwkks687@gmail.com>
2026-05-16 01:38:14 +0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './SmartCametti';
+import App from './SmartCametti_Branded'; // Naye file name ke mutabiq change kar diya
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
